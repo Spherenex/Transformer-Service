0e036453176b3d8b41af629e52e7385c322d0bf2
Spherenex <spherenexdeveloper@gmail.com>
2025-05-29 15:37:22 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       {/* <Vibration/> */}
       {/* <LineFallDetectionDashboard /> */}
       {/* <SingleAxis/> */}
-      <TransformerPrediction />
-      {/* <TransformerService /> */}
+      {/* <TransformerPrediction /> */}
+      <TransformerService />
       {/* <JetEngineMonitor /> */}
     </>
   )
